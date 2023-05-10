07626ce9d45da71e1a30a57543ed2d39627eddba
Danna Vanessa Navia Romero <da.navia@udla.edu.co>
2023-05-09 19:18:45 -0500

    Desativar reportes de publicacion ,funcional

--- Web/Models/PublicacionModel.cs
+++ Web/Models/PublicacionModel.cs
@@ -3,11 +3,11 @@ namespace Web.Models
 {
     public class PublicacionModel
     {
-        public int PUBLI_ID { get; set; } = 20;
+        public int PUBLI_ID { get; set; }
         public string PUBLI_Titulo { get; set; }
         public string PUBLI_Descripcion { get; set; }
         public int PUBLI_Estado { get; set; } = 1;
-        public int? PUBLI_Cantidad { get; set; } = null;
+        public int PUBLI_Cantidad { get; set; } = 0;
         public int? PUBLI_Total { get; set; } = null;
         public int FKPER_RealizaPublicacion { get; set; }
         public int FKTIPUBLI_ID { get; set; }
