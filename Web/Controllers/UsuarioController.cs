c7d988979066fe93a9ce697ffa583c5d59d0bbb5
Seito7w7 <brayan.gomez@udla.edu.co>
2023-05-23 05:56:22 -0500

    Creacion informacion personal usuario

--- Web/Controllers/UsuarioController.cs
+++ Web/Controllers/UsuarioController.cs
@@ -6,16 +6,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using MySql.Data.MySqlClient;
 using Web.Datos;
 using Web.Models;
+using Web.Data;
 
 namespace Web.Controllers
 {
     public class UsuarioController : Controller
     {
         private readonly MySqlConnection connection;
+        private readonly Contexto contexto;
 
-        public UsuarioController(MySqlConnection connection)
+        public UsuarioController(Contexto contexto)
         {
-            this.connection = connection;
+            this.contexto = contexto;
+            connection = new MySqlConnection(contexto.Conexion);
         }
         // GET: UsuarioController
         [Authorize]
@@ -35,6 +38,47 @@ namespace Web.Controllers
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login", "Login");
+        }
+
+        public ActionResult InformacionUsuario()
+        {
+            ContenidoModel contenido = new ContenidoModel();
+            contenido.persona.PER_Correo = Request.Cookies["CorreoPersona"];
+            try
+            {
+                using (MySqlConnection  connection = new MySqlConnection(contexto.Conexion))
+                {
+                    connection.Open();
+                    using (MySqlCommand command = new("mostrarDatosUsuario", connection)) {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@correo", contenido.persona.PER_Correo);
+
+                        using(MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                contenido.persona.PER_NombreUno = reader.GetString(0);
+                                contenido.persona.PER_NombreDos = reader.GetString(1);
+                                contenido.persona.PER_ApellidoUno = reader.GetString(2);
+                                contenido.persona.PER_ApellidoDos = reader.GetString(3);
+                                contenido.persona.PER_Correo = reader.GetString(4);
+                                contenido.persona.PER_DireccionVinda = reader.GetString(5);
+                                contenido.persona.PER_NumeroDocumento = reader.GetInt32(6);
+                                contenido.tipoDocumento.TIPDOC_Nombre = reader.GetString(7);
+                                contenido.pais.PAIS_Nombre = reader.GetString(8);
+                                contenido.departamento.DEP_Nombre = reader.GetString(9);
+                                contenido.municipio.MUN_Nombre = reader.GetString(10);
+                                contenido.barrio.BAR_Nombre = reader.GetString(11);
+                            }
+                        }
+                    }
+
+                }
+            }catch (Exception ex)
+            {
+
+            }
+            return View();
         }
     }
 }
--- Web/Models/ContenidoModel.cs
+++ Web/Models/ContenidoModel.cs
@@ -9,7 +9,13 @@
         public List<TipoRazaModel>? tiposRaza { get; set; }
         public List<TipoMascotaModel>? tiposMascota { get; set; }
         public List<PublicacionModel>? publicaciones { get; set; }
+        public List<PerRegistraMascModel>? perregistraMasc { get; set; }
         public PersonaModel persona { get; set; } = null!;
+        public TipoDocumentoModel tipoDocumento { get; set; }
+        public PaisModel pais { get; set; }
+        public DepartamentoModel departamento { get; set; }
+        public MunicipioModel municipio { get; set; }
+        public BarrioModel barrio { get; set; }
         public PublicacionModel? publicacion { get; set; }
         public ElementosModel? elementos { get; set; }
         public TipoPublicacionModel? tipoPublicacion { get; set; }
@@ -17,7 +23,6 @@
         public MascTieneVacModel mascTieneVac { get; set; } = null!;
         public MascTieneAlerModel MascTieneAler { get; set; } = null!;
         public CirugiaModel cirugia { get; set; } = null!;
-        public List<PerRegistraMascModel>? perregistraMasc { get; set; }
         public byte[] Imagen { get; set; }
 
     }
